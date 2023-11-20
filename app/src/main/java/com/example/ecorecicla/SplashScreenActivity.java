3702b15bbeaebee9ca147c04e43efc9bee56659e
Diego F Cardona <diego.f.cardona.s@gmail.com>
2023-11-20 15:15:11 -0500

    Creacion y adaptacion Splash screen

--- app/src/main/java/com/example/ecorecicla/SplashScreenActivity.java
+++ app/src/main/java/com/example/ecorecicla/SplashScreenActivity.java
@@ -1,2 +1,28 @@
-package com.example.ecorecicla;public class SplashScreenActivity {
+package com.example.ecorecicla;
+
+// SplashScreenActivity.java
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 2000; // Duración de la pantalla de presentación en milisegundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // Utiliza un Handler para abrir la actividad de inicio de sesión después de un tiempo de espera
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Cierra la actividad actual (SplashScreenActivity)
+            }
+        }, SPLASH_TIME_OUT);
+    }
 }
